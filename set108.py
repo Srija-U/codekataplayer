6068484064ce4ca012fc7e98e9e95fa13e612b72
Srija U <48713553+Srija-U@users.noreply.github.com>
2019-07-06 23:13:09 -0300

    Create set108.py

---
+++ set108.py
@@ -0,0 +1,15 @@
+s,a=map(str,input().split())
+f=0
+a=int(a)
+s=list(s)
+for i in range(a+1):
+    if(str(i) in s):
+        f=1
+        continue
+    else:
+        f=0
+        break
+if(f==1):
+    print("yes")
+else:
+    print("no")
